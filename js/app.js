8e8028d4a7292d9d1da4592fde7bcccfd5f69e89
Nicolás Quinero <quinterorodrigueznicolas@gmail.com>
2022-01-10 15:59:30 -0500

    header-list mobile

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const $menu = document.querySelector(".fas.fa-bars");
+const $close = document.querySelector(".fas.fa-times");
+const $headerMobileList = document.querySelector(".headerMobile-list");
+console.log($headerMobileList);
+
+$headerMobileList.style.opacity = '0';
+
+$menu.addEventListener('click', (e)=>{
+  $menu.classList.toggle('none');
+  $close.classList.toggle('none');
+  $headerMobileList.style.opacity = '1';
+  $headerMobileList.style.transition = '.2s opacity linear';
+});
+
+$close.addEventListener('click', ()=>{
+  $close.classList.toggle('none');
+  $menu.classList.toggle('none');
+  $headerMobileList.style.opacity = '0';
+})
